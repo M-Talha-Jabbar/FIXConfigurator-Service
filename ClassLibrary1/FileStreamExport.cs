9cc1f5db9d46b7554f4bf180dc3cb142253bcd66
anaafjawed <105201914+anaafjawed@users.noreply.github.com>
2022-11-03 16:02:29 +0500

    added static FileStreamExport class
    This class is responsible to read and export log file when requested from web client

---
+++ ClassLibrary1/FileStreamExport.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Threading;
+
+namespace FIXMonitorBusinessLogicLayer
+{
+    class FileStreamExport
+    {
+
+        public static Stream fsExport(string filePath) {
+
+            object lockObject = FixMessageLog.GetLockObject(filePath);
+
+            lock (lockObject)
+            {
+                try
+            {
+                    using (FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read)) {
+                        Console.WriteLine("file read");
+                        return fs;
+                    }
+                        
+                }
+                catch (Exception ex) {
+
+                    Console.WriteLine("file reading lock error: ", ex.Message);
+                    return null;
+                }
+
+            }
+        }
+    }
+}
