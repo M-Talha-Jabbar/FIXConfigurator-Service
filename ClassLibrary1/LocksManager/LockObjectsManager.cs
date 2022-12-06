fd6ee749147e839067d3de11791d4d33e43fddcb
anaafjawed <105201914+anaafjawed@users.noreply.github.com>
2022-12-06 21:05:35 +0500

    seperated lock aquiring logic into another class

---
+++ ClassLibrary1/LocksManager/LockObjectsManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIXMonitorBusinessLogicLayer.LocksManager
+{ 
+    class LockObjectsManager
+    {
+        private readonly ConcurrentDictionary<string, object> LockObj = new ConcurrentDictionary<string, object>();
+
+        public object GetLockObj(string filePath)
+        {
+            object lockForThisFile;
+            if (LockObj.ContainsKey(filePath))
+            {
+                lockForThisFile = LockObj[filePath];
+            }
+            else
+            {
+                lockForThisFile = new object();
+                LockObj.TryAdd(filePath, lockForThisFile);
+            }
+            
+            return lockForThisFile;
+        }
+    }
+}
