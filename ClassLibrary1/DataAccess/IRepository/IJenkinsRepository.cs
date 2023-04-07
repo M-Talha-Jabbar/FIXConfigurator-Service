f33eb93dae96dae45548cb4b45cfd838a002d4d6
anaafjawed <105201914+anaafjawed@users.noreply.github.com>
2023-04-07 22:58:52 +0500

    added Repository contract

---
+++ ClassLibrary1/DataAccess/IRepository/IJenkinsRepository.cs
@@ -0,0 +1,17 @@
+﻿using FIXMonitorBusinessLogicLayer.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIXMonitorBusinessLogicLayer.DataAccess.IRepository
+{
+    public interface IJenkinsRepository
+    {
+        Task<FixEngineJenkinsConfiguration> CreateJenkinsConfigAsync(FixEngineJenkinsConfiguration fixEngineJenkinsConfiguration, FIXMonitorContext fixMonitorContext);
+        FixEngineJenkinsConfiguration UpdateJenkinsConfigAsync(FixEngineJenkinsConfiguration fixEngineJenkinsConfiguration, FIXMonitorContext fixMonitorContext);
+        Task<FixEngineJenkinsConfiguration> GetJenkinsConfigAsync(string FixEngineIpAndPort, FIXMonitorContext fixMonitorContext);
+        bool DeleteJenkinsConfigAsync(FixEngineJenkinsConfiguration fixEngineJenkinsConfiguration, FIXMonitorContext fixMonitorContext);
+    }
+}
