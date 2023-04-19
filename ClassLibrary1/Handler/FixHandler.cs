2e13481a513bc2cc64c6c803aaa8ca4c372e8187
anaafjawed <105201914+anaafjawed@users.noreply.github.com>
2023-04-19 14:56:34 +0500

    logging

--- ClassLibrary1/Handler/FixHandler.cs
+++ ClassLibrary1/Handler/FixHandler.cs
@@ -768,6 +768,7 @@ namespace FIXMonitorBusinessLogicLayer.Handler
             }
             catch (Exception e)
             {
+                CoreLogging.Logging.LogMessage(CoreLogging.LOGTYPE.Error, $"Redis Get Connection: {e.Message}");
                 fixEngines.Remove(fixEngine);
                 throw e;
             }
@@ -830,6 +831,7 @@ namespace FIXMonitorBusinessLogicLayer.Handler
             }
             catch (Exception e)
             {
+                CoreLogging.Logging.LogMessage(CoreLogging.LOGTYPE.Error, $"Redis after connection : {e.Message}");
                 LogException(e);
             }
 
